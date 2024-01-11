09097e2988ebd20c9c7c4981468890a3b22b4a64
Linderman Moura <linderman.moura@outlook.com>
2024-01-11 14:51:02 -0300

    H2 database, test profile, JPA

--- src/main/java/com/requiemdev/course/entities/User.java
+++ src/main/java/com/requiemdev/course/entities/User.java
@@ -3,9 +3,18 @@ package com.requiemdev.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
